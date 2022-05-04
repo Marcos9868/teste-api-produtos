b53ccc756806e16933598de968d1d5c221371053
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-04 16:21:07 -0300

    Repositório de usuários, login não implementado

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -25,6 +25,57 @@ class userRepository {
     return user || []
   }
 
+  async createUser(user) {
+    const script = `
+      INSERT INTO users_list 
+      (name, email, password)
+      VALUES ('$1', '$2', crypt('$3', 'my_salt'))
+      RETURNING uuid
+    `
+
+    const values = [user.name, user.email, user.password]
+    const { rows } = await db.query(script, values)
+    const [ newUser ] = rows
+
+    return newUser.uuid
+  }
+
+  async loginUser() {
+
+  }
+
+  async updateUser(user) {
+    const script = `
+      UPDATE users_list SET
+      name = $1,
+      email = $2,
+      password = crypt('$3', 'my_salt')
+      WHERE uuid = $4
+    `
+
+    const values = [user.name, user.email, user.password, user.uuid]
+    await db.query(script, values)
+  }
+
+  async removeUser(uuid) {
+    const script = `
+      DELETE FROM users_list
+      WHERE uuid = $1
+    `
+
+    const values = [uuid]
+
+    await db.query(script, values)
+  }
+
+  async removeUsers() {
+    const script = `
+      TRUNCATE TABLE users_list
+    `
+
+    await db.query(script)
+  }
+
 }
 
 module.exports = new userRepository()
