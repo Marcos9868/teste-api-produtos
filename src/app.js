2b57e46202b6db94c415508727582ef759aff345
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-04 15:28:37 -0300

    Adiciona comentários e importa rota de usuários

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+// Importação dos módulos da API
 const express = require('express')
 const productsRoute = require('./routes/products.route')
 const statusRoute = require('./routes/status.route')
+const usersRoute = require('./routes/users.route')
 
+// Instância do Express
 const app = express()
 
 //Configuração DB
@@ -13,7 +16,9 @@ app.use(express.urlencoded({ extended: true }))
 // Rotas
 app.use(statusRoute)
 app.use(productsRoute)
+app.use(usersRoute)
 
+// Porta do servidor
 app.listen(4000, () => {
   console.log('Servidor em http://localhost:4000')
 })
