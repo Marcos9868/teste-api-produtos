db304704a42564a797f67c327c337132fae4fbf0
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-04 15:26:15 -0300

    Adiciona criação de tabela de usuários e Insere um usuário de exemplo

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS 'uuid-oosp'
+CREATE EXTENSION IF NOT EXISTS 'pgcrypto'
 
 CREATE TABLE IF NOT EXISTS products_list(
   uuid uuid DEFAULT uuid_generate_v4(),
@@ -6,6 +7,14 @@ CREATE TABLE IF NOT EXISTS products_list(
   description VARCHAR NOT NULL,
   price VARCHAR NOT NULL,
   quantity INTEGER,
+  PRIMARY KEY (uuid)
+)
+
+CREATE TABLE IF NOT EXISTS users_list(
+  uuid uuid DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
   PRIMARY KEY (uuid)
 )
 
@@ -19,4 +28,14 @@ INSERT INTO products_list (
   'Placa Nvidia GeForce GTX 770 2GB', 
   'R$1.500,00', 
   2
+)
+
+INSERT INTO users_list (
+  name, 
+  email, 
+  password
+) VALUES (
+  'Marcos Melo Ferreira',
+  'marcos@teste.com',
+  crypt('teste1234', 'my_salt')
 )
