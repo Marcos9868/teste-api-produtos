b6c0067bf5a48d3c4de463f6b424230ce5049a23
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-04 14:51:22 -0300

    Modelos de produto e usuário

---
+++ src/models/product.model.js
@@ -0,0 +1,9 @@
+const Product = (uuid, name, description, price, quantity) => {
+  this.uuid = uuid,
+  this.name = name,
+  this.description = description,
+  this.price = price,
+  this.quantity = quantity
+}
+
+module.exports = Product
