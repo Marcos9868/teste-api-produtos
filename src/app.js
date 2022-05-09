d974d3fa532b6150607bdd77d0816ba7bd1e004e
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-09 11:16:00 -0300

    Adiciona o errorHandler na API

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const productsRoute = require('./routes/products.route')
 const statusRoute = require('./routes/status.route')
 const usersRoute = require('./routes/users.route')
+const errorHandler = require('./middlewares/error.handler.middleware')
 
 // Instância do Express
 const app = express()
@@ -14,9 +15,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Rotas
-app.use(statusRoute)
-app.use(productsRoute)
-app.use(usersRoute)
+app.use(statusRoute) // Status da API
+app.use(productsRoute)  // Rota de produtos
+app.use(usersRoute)  // Rota de usuários
+app.use(errorHandler)  // Rota dos modelos de erros
 
 // Porta do servidor
 app.listen(4000, () => {
